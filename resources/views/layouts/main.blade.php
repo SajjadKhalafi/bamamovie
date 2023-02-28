d42a9eca4f624a853dc092e435a7b732b9c19419
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2023-02-28 22:11:12 +0330

    install breeze

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,174 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie App</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+{{--    <livewire:styles/>--}}
+{{--    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>--}}
+</head>
+<body class="font-sans bg-gray-900 text-white">
+{{--<nav :class="{'flex': open, 'hidden': !open}" class="flex-col flex-grow pb-4 md:pb-0 hidden md:flex md:justify-end md:flex-row">--}}
+{{--    <a class="px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-gray-200 rounded-lg dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Blog</a>--}}
+{{--    <a class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Portfolio</a>--}}
+{{--    <a class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">About</a>--}}
+{{--    <a class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Contact</a>--}}
+{{--    <div @click.away="open = false" class="relative" x-data="{ open: false }">--}}
+{{--        <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">--}}
+{{--            <span>Dropdown</span>--}}
+{{--            <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>--}}
+{{--        </button>--}}
+{{--        <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">--}}
+{{--            <div class="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">--}}
+{{--                <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Link #1</a>--}}
+{{--                <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Link #2</a>--}}
+{{--                <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Link #3</a>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</nav>--}}
+<nav class="border-b border-gray-800">
+    <div class="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between px-4 py-6">
+        <ul class="flex flex-col md:flex-row items-center">
+            <li>
+                <a href="">
+                    <svg class="w-32" viewBox="0 0 96 24" fill="none">
+                        <path
+                            d="M18 4l2 4h-3l-2-4h-2l2 4h-3l-2-4H8l2 4H7L5 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4h-4zM35.568 7.047l2.557 7.219 2.543-7.22h2.693V17h-2.057v-2.72l.205-4.697L38.822 17h-1.408l-2.68-7.41.206 4.69V17h-2.051V7.047h2.68zm9.147 6.186c0-.733.141-1.387.424-1.962a3.108 3.108 0 011.216-1.333c.534-.314 1.151-.471 1.853-.471.998 0 1.812.305 2.44.916.634.61.987 1.44 1.06 2.488l.014.506c0 1.135-.317 2.046-.95 2.734-.634.684-1.484 1.026-2.55 1.026-1.067 0-1.919-.342-2.557-1.026-.633-.683-.95-1.613-.95-2.789v-.089zm1.975.144c0 .702.133 1.24.397 1.613.264.37.642.554 1.135.554.478 0 .852-.182 1.12-.547.27-.37.404-.957.404-1.764 0-.688-.134-1.221-.403-1.6-.27-.377-.647-.567-1.135-.567-.483 0-.857.19-1.121.568-.264.373-.397.954-.397 1.743zm8.908 1.21l1.374-4.983h2.064L56.541 17h-1.887L52.16 9.604h2.065l1.374 4.983zM61.996 17h-1.982V9.604h1.982V17zm-2.099-9.31c0-.297.098-.54.294-.732.2-.191.472-.287.814-.287.337 0 .606.096.806.287.201.191.301.435.301.731 0 .301-.102.547-.307.739-.2.191-.467.287-.8.287s-.602-.096-.807-.287a.975.975 0 01-.3-.739zm7.137 9.447c-1.085 0-1.969-.333-2.652-.998-.68-.666-1.019-1.552-1.019-2.66v-.19c0-.744.144-1.407.43-1.99a3.143 3.143 0 011.218-1.354c.528-.319 1.13-.478 1.804-.478 1.012 0 1.807.319 2.386.957.584.638.875 1.543.875 2.714v.806h-4.71c.064.483.255.87.574 1.162.324.292.732.438 1.224.438.761 0 1.356-.276 1.784-.827l.97 1.087a2.99 2.99 0 01-1.202.984 3.98 3.98 0 01-1.682.349zm-.225-6.07c-.392 0-.711.132-.957.396-.242.264-.397.643-.465 1.135h2.748v-.158c-.01-.437-.128-.774-.356-1.011-.228-.242-.551-.363-.97-.363zm10.144 3.882h-3.596L72.674 17h-2.18l3.704-9.953h1.9L79.825 17h-2.18l-.69-2.05zm-3.042-1.66H76.4l-1.25-3.726-1.238 3.725zm13.043.081c0 1.14-.26 2.053-.78 2.741-.514.684-1.211 1.026-2.091 1.026-.747 0-1.351-.26-1.811-.78v3.487h-1.976V9.604h1.832l.068.724c.479-.574 1.103-.861 1.873-.861.912 0 1.62.337 2.126 1.011.506.675.76 1.605.76 2.79v.102zm-1.975-.143c0-.689-.123-1.22-.37-1.593-.241-.374-.594-.56-1.06-.56-.619 0-1.045.236-1.278.71v3.028c.242.488.673.732 1.293.732.943 0 1.415-.773 1.415-2.317zm9.864.143c0 1.14-.26 2.053-.78 2.741-.514.684-1.212 1.026-2.091 1.026-.748 0-1.352-.26-1.812-.78v3.487h-1.975V9.604h1.832l.068.724c.479-.574 1.103-.861 1.873-.861.912 0 1.62.337 2.126 1.011.506.675.759 1.605.759 2.79v.102zm-1.976-.143c0-.689-.123-1.22-.369-1.593-.242-.374-.595-.56-1.06-.56-.62 0-1.045.236-1.278.71v3.028c.242.488.672.732 1.292.732.944 0 1.415-.773 1.415-2.317z"
+                            fill="#fff"/>
+                    </svg>
+                </a>
+            </li>
+            <li class="md:ml-16 mt-3 md:mt-0">
+                <div class="relative" x-data="{ openMovies: false }">
+                    <button @click="openMovies = !openMovies"
+                            class="flex flex-row items-center">
+                        <span>Movies</span>
+                    </button>
+                    <div x-show="openMovies" @click.away="openMovies = false"
+                         x-transition:enter="transition ease-out duration-100"
+                         x-transition:enter-start="transform opacity-0 scale-95"
+                         x-transition:enter-end="transform opacity-100 scale-100"
+                         x-transition:leave="transition ease-in duration-75"
+                         x-transition:leave-start="transform opacity-100 scale-100"
+                         x-transition:leave-end="transform opacity-0 scale-95"
+                         class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48 z-50">
+                        <div class="px-2 py-2 bg-white rounded-md">
+                            <a class="block px-4 py-2 mt-2 text-sm font-semibold rounded-lg
+                             md:mt-0 text-gray-900 focus:text-gray-900 bg-gray-200 focus:bg-gray-200 focus:outline-none
+                             focus:shadow-outline"
+                               href="">Popular</a>
+                            <a class="block px-4 py-2 mt-2 text-sm font-semibold rounded-lg
+                             md:mt-0 text-gray-900 focus:text-gray-900 bg-gray-200 focus:bg-gray-200 focus:outline-none
+                             focus:shadow-outline"
+                               href="">Now Playing</a>
+                            <a class="block px-4 py-2 mt-2 text-sm font-semibold rounded-lg
+                             md:mt-0 text-gray-900 focus:text-gray-900 bg-gray-200 focus:bg-gray-200 focus:outline-none
+                             focus:shadow-outline"
+                               href="">Upcoming</a>
+                            <a class="block px-4 py-2 mt-2 text-sm font-semibold rounded-lg
+                             md:mt-0 text-gray-900 focus:text-gray-900 bg-gray-200 focus:bg-gray-200 focus:outline-none
+                             focus:shadow-outline"
+                               href="">Top Rated</a>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="md:ml-6 mt-3 md:mt-0">
+                <div class="relative" x-data="{ openTVShows: false }">
+                    <button @click="openTVShows = !openTVShows"
+                            class="flex flex-row items-center">
+                        <span>TV Shows</span>
+                    </button>
+                    <div x-show="openTVShows" @click.away="openTVShows = false"
+                         x-transition:enter="transition ease-out duration-100"
+                         x-transition:enter-start="transform opacity-0 scale-95"
+                         x-transition:enter-end="transform opacity-100 scale-100"
+                         x-transition:leave="transition ease-in duration-75"
+                         x-transition:leave-start="transform opacity-100 scale-100"
+                         x-transition:leave-end="transform opacity-0 scale-95"
+                         class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48 z-50">
+                        <div class="px-2 py-2 bg-white rounded-md">
+                            <a class="block px-4 py-2 mt-2 text-sm font-semibold rounded-lg
+                             md:mt-0 text-gray-900 focus:text-gray-900 bg-gray-200 focus:bg-gray-200 focus:outline-none
+                             focus:shadow-outline"
+                               href="">Popular</a>
+                            <a class="block px-4 py-2 mt-2 text-sm font-semibold rounded-lg
+                             md:mt-0 text-gray-900 focus:text-gray-900 bg-gray-200 focus:bg-gray-200 focus:outline-none
+                             focus:shadow-outline"
+                               href="">Airing Today</a>
+                            <a class="block px-4 py-2 mt-2 text-sm font-semibold rounded-lg
+                             md:mt-0 text-gray-900 focus:text-gray-900 bg-gray-200 focus:bg-gray-200 focus:outline-none
+                             focus:shadow-outline"
+                               href="">On TV</a>
+                            <a class="block px-4 py-2 mt-2 text-sm font-semibold rounded-lg
+                             md:mt-0 text-gray-900 focus:text-gray-900 bg-gray-200 focus:bg-gray-200 focus:outline-none
+                             focus:shadow-outline"
+                               href="">Top Rated</a>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="md:ml-6 mt-3 md:mt-0">
+                <a href="" class="hover:text-gray-300">Actors</a>
+            </li>
+        </ul>
+        <div class="flex flex-col md:flex-row items-center">
+            {{--            <div class="md:ml-4 mt-3 md:mt-0">--}}
+            {{--                <a href="#">--}}
+            {{--                    <img src="{{ asset('avatar.jpg') }}" alt="avatar" class="rounded-full w-8 h-8">--}}
+            {{--                </a>--}}
+            {{--            </div>--}}
+            @auth
+                <div class="hidden sm:flex sm:items-center sm:ml-6">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button
+                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                <div>{{ Auth::user()->name }}</div>
+
+                                <div class="ml-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                         viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd"
+                                              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                              clip-rule="evenodd"/>
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            <x-dropdown-link :href="route('profile.edit')">
+                                {{ __('Profile') }}
+                            </x-dropdown-link>
+
+                            <!-- Authentication -->
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+
+                                <x-dropdown-link :href="route('logout')"
+                                                 onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                    {{ __('Log Out') }}
+                                </x-dropdown-link>
+                            </form>
+                        </x-slot>
+                    </x-dropdown>
+                </div>
+            @endauth
+
+            @guest
+                <a href="{{ route('login') }}" class="mx-4">
+                    Login
+                </a>
+            @endguest
+        </div>
+    </div>
+</nav>
+@yield('content')
+{{--<livewire:scripts/>--}}
+@yield('scripts')
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,45 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-</head>
-<body>
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
-</body>
-</html>
+@extends('layouts.main')
+
+@section('content')
+
+    <section
+        class="container mx-auto bg-cover">
+        <div class="max-w-screen-xl lg:py-16 lg:px-12">
+            <div class="w-full mb-[20px]">
+                <h2 class="text-[3rem] font-[700] text-white md:text-5xl lg:text-6xl">Welcome.</h2>
+                <p class="text-[2rem] font-[600] text-white">Millions of movies, TV shows and people to discover.
+                    Explore now.</p>
+            </div>
+
+
+            <form>
+                <div class="flex">
+                    <div class="relative w-full">
+                        <input type="search" id=location-search"
+                               class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+                               placeholder="Search for a movie, tv show, person......" required>
+                        <button type="submit"
+                                class="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <svg aria-hidden="true" class="w-5 h-5" fill="none" stroke="currentColor"
+                                 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                            </svg>
+                            <span class="sr-only">Search</span>
+                        </button>
+                    </div>
+                </div>
+            </form>
+
+
+        </div>
+    </section>
+
+{{--    @include('landing.trending')--}}
+
+{{--    @include('landing.trailers')--}}
+
+{{--    @include('landing.populars')--}}
+
+{{--    @include('landing.free-to-watch')--}}
+@endsection
